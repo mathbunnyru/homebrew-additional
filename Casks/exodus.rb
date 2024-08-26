a40855da2aea1ea63e73a84533745fb2a19827d3
Ayaz Salikhov <mathbunnyru@gmail.com>
2024-08-26 14:22:28 +0100

    Update exodus to 24.33.4

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,9 +1,9 @@
 cask "exodus" do
   arch arm: "macos-arm64", intel: "macos"
 
-  version "24.31.4"
-  sha256 arm:   "d3d7826d156629d52aa1fdc04f325b69e5502cf2400a347e21bf7a3d1901f2b5",
-         intel: "71b5a2caea3edd064155daf9790c016d5703fb648089c09efae05eec9c6b2dfa"
+  version "24.33.4"
+  sha256 arm:   "3611039fc81a8ee658e19e75f2e24f62623a7e455442cca759347de8166620c3",
+         intel: "ed28ccd53d174f626155b866ebc5e0f42493ccc89a12ac01a4fcc3e67e4932be"
 
   url "https://downloads.exodus.com/releases/exodus-#{arch}-#{version}.dmg",
       user_agent: :fake
