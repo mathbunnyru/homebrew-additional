8501368adfefed11fe1c150bb7d09caa53401701
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-06-10 08:54:13 +0100

    Fix style

--- Casks/decibel.rb
+++ Casks/decibel.rb
@@ -3,7 +3,8 @@ cask "decibel" do
   sha256 "c9788e547b09ba59bdef8abf4c634f9796823ef7f984691c608f2d37e92433f2"
 
   # s3.amazonaws.com/feisty-dog/ was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/feisty-dog/Decibel-#{version}.dmg", verified: "s3.amazonaws.com/feisty-dog/"
+  url "https://s3.amazonaws.com/feisty-dog/Decibel-#{version}.dmg",
+      verified: "s3.amazonaws.com/feisty-dog/"
   name "Decibel"
   desc "Audio player tailored to the particular needs of audiophiles"
   homepage "https://www.feisty-dog.com/decibel/"
