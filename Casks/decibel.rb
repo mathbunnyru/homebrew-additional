9e2a03c0ff38416ed949b5bee8ee2c01008ec196
Ayaz Salikhov <mathbunnyru@gmail.com>
2025-09-30 08:46:39 +0100

    Update Decibel to 2.0b36

--- Casks/decibel.rb
+++ Casks/decibel.rb
@@ -1,6 +1,6 @@
 cask "decibel" do
-  version "2.0b35"
-  sha256 "c9788e547b09ba59bdef8abf4c634f9796823ef7f984691c608f2d37e92433f2"
+  version "2.0b36"
+  sha256 "86bd338e5d5244b19631771e124733a1927292e9d56d8391093759d810dffe71"
 
   # s3.amazonaws.com/feisty-dog/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/feisty-dog/Decibel-#{version}.dmg",
