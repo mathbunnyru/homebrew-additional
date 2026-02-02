5c24de65bf3741e93365fe3ce024a64a0bdadf1b
mathbunnyru <12270691+mathbunnyru@users.noreply.github.com>
2026-02-02 00:18:28 +0000

    Update exodus version and hashes

--- Casks/exodus.rb
+++ Casks/exodus.rb
@@ -1,9 +1,9 @@
 cask "exodus" do
   arch arm: "macos-arm64", intel: "macos"
 
-  version "26.1.5"
-  sha256 arm:   "6073577bc0dfd578847457fc1bf73c616a4b44f064876af632c3a1618ef99782",
-         intel: "06c13788a614a348c4e04d1946a2892c4e63063e592b4f5afaaebdbb052c5e03"
+  version "26.1.30"
+  sha256 arm:   "e2bd3b2de33c85775e5928e7867b6f0e362762610fa109b341381e5b812000d1",
+         intel: "f6e9fa4991fbaa646f9cf4b759cb9e1030d95ba115951445d1467280cf062445"
 
   url "https://downloads.exodus.com/releases/exodus-#{arch}-#{version}.dmg",
       user_agent: :fake
